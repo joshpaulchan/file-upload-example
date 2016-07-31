c20045ee1168151b55624f22abb1655c369f7869
joshpaulchan <joshpaulchan@gmail.com>
2016-07-30 20:28:29 -0400

    Fix uploading to s3

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
+
+var aws = require('aws-sdk');
 
 // require multer
 var multer = require('multer');
-
-// create multer object
-var upload = multer({ dest: 'uploads/' });
+var multerS3 = require('multer-s3');
 
 // require s3
-var s3 = require('s3');
-
 var options = {
-  multipartUploadThreshold: 20971520, // this is the default (20 MB) 
-  multipartUploadSize: 15728640, // this is the default (15 MB) 
-  s3Options: {
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_KEY,
-    // region: "oregon",
-    // NEEDED for Buckets in Frankfurt (and change region)
-    // signatureVersion: 'v4',
-    // s3DisableBodySigning: true
-    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
-  }
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_KEY,
+  logger: console
+  // region: "oregon",
+  // NEEDED for Buckets in Frankfurt (and change region)
+  // signatureVersion: 'v4',
+  // s3DisableBodySigning: true
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
 };
 
 // create s3 client
-var client = s3.createClient(options);
+var s3 = new aws.S3(options);
+
+// create multer object
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'joshmagic',
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -51,38 +57,6 @@ router.post('/url/to/upload/to', upload.single('fileName'), function(req, res, n
   
   // Path of the file that was sent to server
   console.log("Relative path of the new file: ", req.file.path);
-  
-  var params = {
-    // make absolute path
-    localFile: req.file.path,
-    
-    s3Params: {
-      // your s3 bucket name
-      Bucket: process.env.UPLOAD_BUCKET_NAME,
-      // key is the name you want your file to be on S3
-      Key: req.file.path,
-      // other options supported by putObject, except Body and ContentLength. 
-      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property 
-    },
-  };
-  
-  // create upload client
-  var uploader = client.uploadFile(params);
-  
-  // add handler for errors
-  uploader.on('error', function(err) {
-    console.error("unable to upload:", err.stack);
-  });
-  
-  // add handler to show progress
-  uploader.on('progress', function() {
-    console.log("[progress]", uploader.progressMd5Amount, uploader.progressAmount, uploader.progressTotal);
-  });
-  
-  // add handler to show end
-  uploader.on('end', function() {
-    console.log("done uploading");
-  });
   
   res.status(200).json({
     success: true,
